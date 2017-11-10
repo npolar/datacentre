f352234c7dab883319c238cdb61f6f0b0bea67c0
cnrdh <cnrdh@users.noreply.github.com>
2017-11-10 10:21:42 +0100

    Updated

--- workbox-sw-src.js
+++ workbox-sw-src.js
@@ -1,58 +1,29 @@
+// Source for a service worker
+// Run `yarn run sw` to compile the script into the "src" folder
 // https://workboxjs.org/
 // https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook
 
 importScripts('workbox-sw.prod.v2.1.0.js');
-
 const workboxSW = new WorkboxSW({clientsClaim: true, skipWaiting: true});
+
+// Leave the following line with empty array as argument
 workboxSW.precache([]);
 
+// Requests for non-existing resources are routed to /
 workboxSW.router.registerNavigationRoute('index.html');
 
 workboxSW.router.registerRoute(
-  'https://api-test.data.npolar.no/(.*)',
-  workboxSW.strategies.cacheFirst()
-);
-
-workboxSW.router.registerRoute(
-  'https://api.npolar.no/(.*)',
+  'https://cdn.jsdelivr.net/(.*)',
   workboxSW.strategies.cacheFirst()
 );
 
 // Revison-cache for Npolar APIs
-// api.npolar.no && [?&]rev=[0-9]+\-[0-9a-f]{N}
+const npolarApiRE = /^https:\/\/api(-test\.data)?\.npolar\.no\/(.+)[\?\&]rev=(.+)/;
 // Cache-first for Npolar API resources containing a revision (they never change...)
-// workboxSW.router.registerRoute(
-//   /^https:\/\/api\.npolar\.no\/(.+)\?rev=(.+)/,
-//   workboxSW.strategies.cacheFirst()
-// );
+workboxSW.router.registerRoute(
+  npolarApiRE,
+  workboxSW.strategies.cacheFirst()
+);
 
-// Faceting only
-// api.npolar.no && q= && facets && limit === 0
-// => network-first
-
-
-// CDNs: cache-first
-
-
-// On activate, check if there is any newer than ....
-// https://api.npolar.no/dataset/?q=&sort=-updated&limit=1&format=json&variant=array&fields=id,updated
-// function fetchJson(url) {
-//   return fetch(url).then(r => r.json());
-// }
-//
-// function fetchLatestRevisions(endpoint, params = { sort='-updated', limit=20, fields='id,_rev' }={}) {
-//   let url = new URL(`${endpoint}/?q=&sort=${sort}&limit=${limit}&fields=${fields}&format=json&variant=array`, 'https://api.npolar.no');
-//   console.log(url);
-//   return fetchJson(url);
-// }
-//
-// const endpoint = '/dataset';
-// fetchLatestRevisions(endpoint).then(latest => {
-//   latest.forEach(d => {
-//     const id = d.id;
-//     const rev = d._rev;
-//     fetchJson(`https://api.npolar.no${endpoint}/${id}?rev=${rev}`).then(dataset => {
-//       console.log(dataset.updated, dataset.title);
-//     });
-//   });
-// });
+// File-api cacheFirst
+// /_file/*.json|$
